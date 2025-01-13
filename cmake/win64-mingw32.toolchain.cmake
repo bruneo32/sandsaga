f68b30c35751398934360c48976f7af140b9d991
bruneo32 <bruneo32b@gmail.com>
2025-01-13 17:42:50 +0100

    Processor specific optimization flags

--- cmake/win64-mingw32.toolchain.cmake
+++ cmake/win64-mingw32.toolchain.cmake
@@ -5,15 +5,6 @@ set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--subsystem,windows")
 
-# This flags are mandatory in order to work with Ryzen processors
-# (only a problem in Windows)
-set(WINDOWS_COMPAT_FLAGS "-fopenmp -m64 -funroll-loops -msha")
-
-set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${WINDOWS_COMPAT_FLAGS}")
-set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${WINDOWS_COMPAT_FLAGS}")
-set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_C_FLAGS_RELEASE})
-set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_C_FLAGS_DEBUG})
-
 # Set the path to the cross-compiler
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
