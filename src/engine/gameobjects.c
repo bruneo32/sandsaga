14a1a5c87f21c6401cd80129593fe2171e73a4e6
bruneo32 <bruneo32b@gmail.com>
2025-01-28 23:15:54 +0100

    Draw shader for stone

--- src/engine/gameobjects.c
+++ src/engine/gameobjects.c
@@ -26,7 +26,6 @@ GO_ID register_gameobject(GO_Type type, float density, Color color,
 
 static Color C_VAPOR = {0x7F, 0xFF, 0xFF, 0x69};
 static Color C_WATER = {0x7F, 0x7F, 0xFF, 0xAF};
-static Color C_STONE = {0x7F, 0x7F, 0x7F, 0xFF};
 
 static Color C_SAND	 = {0xE2, 0xDB, 0xA4, 0xFF};
 static Color C_SAND2 = {0xFB, 0xF4, 0xBD, 0xFF};
@@ -65,9 +64,44 @@ static void F_draw_sand(size_t wx, size_t wy, int vx, int vy) {
 	Render_Pixel(vx, vy);
 }
 
+static Color C_STONE  = {0x78, 0x7A, 0x79, 0xFF};
+static Color C_STONE2 = {0x70, 0x72, 0x71, 0xFF};
+static Color C_STONE3 = {0x83, 0x85, 0x84, 0xFF};
+static Color C_STONE4 = {0x65, 0x67, 0x66, 0xFF};
+
+/* Draw pattern for stone */
+static void F_draw_stone(size_t wx, size_t wy, int vx, int vy) {
+	/* Pseudo-random seed based only on world coordinates */
+	const size_t seed = wx + (wy ^ wx);
+
+	const double frequency = 3.0; /* Controls the granularity of the noise */
+	const size_t depth	   = 3;	  /* Controls the layers of detail */
+
+	/* Generate a pseudo-random value based on Perlin noise */
+	double noise_value =
+		perlin2d(seed, (double)wx, (double)wy, frequency, depth);
+
+	/* Normalize noise value to a range of 0-1 */
+	noise_value = fmod(noise_value, 1.0);
+	if (noise_value < 0)
+		noise_value += 1.0;
+
+	/* Map noise value to a specific color */
+	if (noise_value > 0.4)
+		Render_Setcolor(C_STONE);
+	else if (noise_value > 0.2)
+		Render_Setcolor(C_STONE2);
+	else if (noise_value > 0.1)
+		Render_Setcolor(C_STONE3);
+	else
+		Render_Setcolor(C_STONE4);
+
+	Render_Pixel(vx, vy);
+}
+
 void init_gameobjects() {
 	GO_VAPOR = register_gameobject(GO_GAS, 0.0f, C_VAPOR, NULL);
 	GO_WATER = register_gameobject(GO_LIQUID, 1.0f, C_WATER, NULL);
 	GO_SAND	 = register_gameobject(GO_POWDER, 2.0f, C_SAND, F_draw_sand);
-	GO_STONE = register_gameobject(GO_STATIC, 3.0f, C_STONE, NULL);
+	GO_STONE = register_gameobject(GO_STATIC, 3.0f, C_STONE, F_draw_stone);
 }
